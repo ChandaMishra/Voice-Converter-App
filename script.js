4a6e5bf867103f814199c0fc3be4914815be0436
ChandaMishra <151567029+ChandaMishra@users.noreply.github.com>
2025-01-23 20:57:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let speech=new SpeechSynthesisUtterance();
+
+let voices=[];
+let voicSelect=document.querySelector("Select");
+window.speechSynthesis.onvoiceschanged=()=>{
+    voices=window.speechSynthesis.getVoices();
+    speech.voice=voices[0];
+    voices.forEach((voice,i)=>(voicSelect.options[i]=new Option(voice.name,i)));
+};
+voicSelect.addEventListener("change",()=>{
+    speech.voice=voices[voicSelect.value]
+});
+
+
+
+
+
+
+document.querySelector("button").addEventListener("click",()=>{
+    speech.text=document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+})
